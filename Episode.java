5c6c0c47f97ce6b37f713b2996ff0ebc7979a10f
nahomsof <nahomsileshi21@gmail.com>
2024-03-30 12:11:02 +0300

    commitJ

---
+++ Episode.java
@@ -0,0 +1,25 @@
+public class Episode {
+    int seriesNumber;
+  int episodeNumber;
+
+  void play() {
+    System.out.println("Playing episode " + seriesNumber);
+  }
+
+  void skipIntro() {
+    System.out.println("Skipping intro...");
+  }
+
+  void skipToNext() {
+    System.out.println("Loading next episode...");
+  }
+}
+
+class EpisodeTestDrive {
+  public static void main(String[] args) {
+    Episode episode = new Episode();
+    episode.seriesNumber = 4;
+    episode.play();
+    episode.skipIntro();
+  }
+}
